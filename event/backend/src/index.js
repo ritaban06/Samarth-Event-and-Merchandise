02e1504546a6984a40e926e9b9869e908b9f2da6
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-06-27 23:01:36 +0530

    Removed custom env

--- event/backend/src/index.js
+++ event/backend/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '.env.backend.event' }); // Load environment variables
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
