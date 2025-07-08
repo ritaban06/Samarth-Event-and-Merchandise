24d3f1013ba599652b19fb6435d90ac06cff02b7
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-07-08 21:08:35 +0530

    Update googleDriveUtils.js

--- event/client/src/utils/googleDriveUtils.js
+++ event/client/src/utils/googleDriveUtils.js
@@ -1,31 +1,22 @@
-export const convertGoogleDriveUrl = (url) => {
-  if (!url) return '';
-
-  try {
-    // Extract file ID from various Google Drive URL formats
-    let fileId = '';
-    const fileIdRegex = /(?:file\/d\/|open\?id=|uc\?id=|d\/|folders\/|id=)([\w-]{25,})/;
-    const match = url.match(fileIdRegex);
-    if (match && match[1]) {
-      fileId = match[1];
-    } else {
-      // Try to extract from sharing link
-      const shareLinkRegex = /https:\/\/drive\.google\.com\/file\/d\/([\w-]{25,})/;
-      const shareMatch = url.match(shareLinkRegex);
-      if (shareMatch && shareMatch[1]) {
-        fileId = shareMatch[1];
-      } else {
-        return url; // Not a recognized Google Drive link
-      }
-    }
-    // Return the direct link format for viewing
-    return `https://drive.google.com/uc?export=view&id=${fileId}`;
-  } catch (error) {
-    console.error('Error converting Google Drive URL:', error);
-    return url;
-  }
+export const isGoogleDriveUrl = (url) => {
+  return url?.includes('drive.google.com');
 };
 
-export const isGoogleDriveUrl = (url) => {
-  return url && url.includes('drive.google.com');
+export const convertGoogleDriveUrl = (url) => {
+  if (!url) return '';
+  
+  if (!isGoogleDriveUrl(url)) {
+    return url;
+  }
+
+  try {
+    const fileId = url.match(/[-\w]{25,}/);
+    if (fileId) {
+      return `https://lh3.googleusercontent.com/d/${fileId[0]}`;
+    }
+  } catch (error) {
+    console.error('Error converting Google Drive URL:', error);
+  }
+  
+  return url;
 };
