70728ecd11e0b36f2300a0e61f30a06f8cd6d35b
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2025-08-23 19:19:16 +0530

    Update index.js

--- event/backend/src/index.js
+++ event/backend/src/index.js
@@ -216,7 +216,7 @@ async function watchEvents() {
         console.log("🔄 Syncing with Google Sheets for events:", eventsToSync);
 
         try {
-          await fetch("https://script.google.com/macros/s/AKfycbxhnpWBBgkIw1y7EGw7AKYXi4MjT-_p34Op6w2GibIQHRm8WH1tZOR0xV72gzAqULU/exec", {
+          await fetch("https://script.google.com/macros/s/AKfycbyOcmJYkj0LHfznPIc8RAQenvPaZxSv1lg2PDC5_9H13lPXQmwX00YqioOxG1e7SknK/exec", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ events: eventsToSync })
